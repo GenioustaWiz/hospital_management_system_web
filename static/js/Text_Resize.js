f64e5bcaa363648ccf38890dfbf4c35bd82a825b
GenioustaWiz <41740162+GenioustaWiz@users.noreply.github.com>
2023-08-07 15:36:31 +0300

    first commit

---
+++ static/js/Text_Resize.js
@@ -0,0 +1,32 @@
+  // Get all the elements that contain text on the page
+  const elements = document.querySelectorAll('body *:not(script)');
+
+  // Function to set the font size of an element
+  const setFontSize = (element, size) => {
+    element.style.fontSize = `${size}px`;
+  };
+  
+  // Function to resize all text on the page based on screen size
+  const resizeText = () => {
+    // Get the current screen size
+    const screenWidth = window.innerWidth;
+  
+    // Set the base font size based on screen size
+    let baseSize;
+    if (screenWidth >= 768) {
+      baseSize = 14;
+    } else {
+      baseSize = 12;
+    }
+  
+    // Loop through each element and set its font size
+    elements.forEach((element) => {
+      setFontSize(element, baseSize);
+    });
+  };
+  
+  // Call the resizeText function when the window is resized
+  window.addEventListener('resize', resizeText);
+  
+  // Call the resizeText function on page load
+  resizeText();
---
+++ static/js/loading.js
@@ -0,0 +1,6 @@
+// Wait for the entire website to load
+window.addEventListener('load', function () {
+    // Hide the loading animation by removing the loading container element
+    var loadingContainer = document.getElementById('preloader');
+    loadingContainer.style.display = 'none';
+});
