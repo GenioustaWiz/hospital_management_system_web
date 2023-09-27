39e66fc9ef62e82e8c5932769378b988b7fa20f6
GenioustaWiz <cm0215557@gmail.com>
2023-09-27 07:01:23 +0300

    new

--- hospital_website/urls.py
+++ hospital_website/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from .views import *
 from .admin_views.dashboard_home_V import *
+from .admin_views.content_views.homeP_info_view_V import home_page_view
 from .admin_views.editing_views.homeP_infor_edit_V import *
 from .admin_views.editing_views.homeP_cards_edit_V import *
 from .admin_views.editing_views.aboutP_V import *
@@ -9,7 +10,6 @@ from .admin_views.editing_views.ContactSidebarCompanyInfo_edit_V import *
 from .admin_views.editing_views.information_footer_edit_V import *
 from .admin_views.content_views.aboutP_view import *
 from .admin_views.content_views.ContactSidebarCompanyInfo_view_V import *
-from .admin_views.content_views.homeP_info_view_V import home_page_view
 from .admin_views.content_views.top_footer_view_V import *
 from .ajax_common_data_send_V import *
 # from .admin_views.content_views.
